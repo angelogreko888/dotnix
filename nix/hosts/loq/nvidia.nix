798c0b50882fa20bfcc0fc5f81c8eb21770dd2f2
angelogreko <angelogreko@gmail.com>
2025-06-13 13:02:41 +0300

    commit

--- nix/hosts/loq/nvidia.nix
+++ nix/hosts/loq/nvidia.nix
@@ -6,7 +6,7 @@
 
 boot = {
 #    initrd.systemd.dbus.enable = true;
-    kernelParams = [ "i915" "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" "nvidia-drm.fbdev=1" ];
+    kernelParams = [ "i915.enable_guc=2" "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" "nvidia-drm.fbdev=1" ];
   };
 
 #hardware = {
