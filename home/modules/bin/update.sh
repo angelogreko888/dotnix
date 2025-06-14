fde5a599144e7bc3d21bb51fd6a19c6b5ac993b9
angelogreko <angelogreko@gmail.com>
2025-06-14 10:02:47 +0300

    commit

---
+++ home/modules/bin/update.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+handle_error() {
+      echo "An error occurred on line $1"
+      exit 1
+  }
+
+  trap 'handle_error $LINENO' ERR
+
+a=$(date | awk '{print $1}')
+if [ $a == Sat ];then
+	flag="/var/tmp/$(basename -- $0).flag"
+	if [ -e "$flag" ] ;then
+	[ $(date +%F) = $(date +%F -r "$flag") ] && exit 1
+	fi
+	touch "$flag"
+                exec foot sh -c '
+		printf "\n\e[1;32m....... update ....... \n\n\e[0m"  
+		cd ~/dotnix/home
+		nix flake update
+		cd ~/dotnix/nix
+		nix flake update
+		nix-channel --update
+		~/bin/switch.sh
+		echo ~~~~~~~~~~~~~~~~Done!!!~~~~~~~~~~~~~~~~; cd ~;  read'
+else
+		if [ -f /var/tmp/trim.sh.flag ]; then
+		rm /var/tmp/trim.sh.flag 
+		fi
+fi
